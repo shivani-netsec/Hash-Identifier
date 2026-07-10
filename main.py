393ce0ecac47b1514f264931251be1f2c29836b1
Shivani557-eng <shivanimourya9955@gmail.com>
2026-07-10 23:49:49 +0530

    Added main menu for hash analysis

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 print("=== Hash Identifier ===")
+print("1. Analyze Single Hash")
+print("2. Analyze Hashes From File")
 
 valid_chars = "0123456789abcdefABCDEF"
 
-hash_value = input("Enter hash: ")
+choice = input("\nChoose an option (1 or 2): ")
+
+if choice == "1":
+    hash_value = input("\nEnter hash: ")
+
 length = len(hash_value)
 
 is_valid = True
@@ -16,32 +22,36 @@ if not is_valid:
 
 else:
     length = len(hash_value)
-    print("\n----- Analysis -----")
-    print("Length:", length) 
+    print("\n" + "=" * 40)
+    print("         HASH ANALYSIS REPORT")
+    print("=" * 40)
+    print("Hash        :", hash_value)
+    print("Length : ", length) 
+    print("Hexadecimal : Yes")
 
     if length == 32:
-        print("Valid MD5 Hash")
-        print("Security:Weak(Collision attack possible)")
+        print("Hash Type : MD5")
+        print("Security : Weak(Collision attack possible)")
 
     elif length == 40:
-        print("Valid SHA-1 Hash")
-        print("Security:Weak(Deprecated)")
+        print("Hash Type : SHA-1")
+        print("Security : Weak(Deprecated)")
 
     elif length == 64:
-        print("Valid SHA-256 Hash")
-        print("Security:Medium(Still secure for most purposes)")
+        print("Hash Type :SHA-256")
+        print("Security : Medium(Still secure for most purposes)")
 
     elif length == 56:
-        print("✅ Valid SHA-224 Hash")
-        print("Security:Medium(Still secure for most purposes)")
+        print("Hash Type : SHA-224")
+        print("Security : Medium(Still secure for most purposes)")
 
     elif length == 96:
-        print("✅ Valid SHA-384 Hash")
-        print("Security:High(Currently secure)")
+        print("Hash Type : SHA-384")
+        print("Security : High(Currently secure)")
 
     elif length == 128:
-        print("✅ Valid SHA-512 Hash")
-        print("Security:High(Currently secure)")
+        print("Hash Type :SHA-512")
+        print("Security : High(Currently secure)")
 
     else:
         print("Valid hexadecimal string, but unknown hash type.")
