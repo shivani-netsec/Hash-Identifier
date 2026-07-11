4f7aba3ee6a0f9c65e31bcf98d756291f3762567
Shivani557-eng <shivanimourya9955@gmail.com>
2026-07-11 22:26:24 +0530

    Refactored hash analyzer with reusable function

--- main.py
+++ main.py
@@ -17,35 +17,42 @@ def analyze_hash(hash_value):
     print("         HASH ANALYSIS REPORT")
     print("=" * 40)
     print("Hash        :", hash_value)
-    print("Length : ", length) 
+    print("Length      : ", length) 
     print("Hexadecimal : Yes")
 
     if length == 32:
-        print("Hash Type : MD5")
-        print("Security : Weak(Collision attack possible)")
+        print("Hash Type  : MD5")
+        print("Security   : Weak(Collision attack possible)")
+        return "MD5"
 
     elif length == 40:
-        print("Hash Type : SHA-1")
+        print("Hash Type  : SHA-1")
         print("Security : Weak(Deprecated)")
+        return "SHA-1"
 
     elif length == 64:
-        print("Hash Type :SHA-256")
-        print("Security : Medium(Still secure for most purposes)")
+        print("Hash Type  : SHA-256")
+        print("Security   : Medium(Still secure for most purposes)")
+        return "SHA-256"
 
     elif length == 56:
-        print("Hash Type : SHA-224")
-        print("Security : Medium(Still secure for most purposes)")
+        print("Hash Type  : SHA-224")
+        print("Security   : Medium(Still secure for most purposes)")
+        return "SHA-224"
 
     elif length == 96:
-        print("Hash Type : SHA-384")
-        print("Security : High(Currently secure)")
+        print("Hash Type  : SHA-384")
+        print("Security   : High(Currently secure)")
+        return "SHA-384"
 
     elif length == 128:
-        print("Hash Type :SHA-512")
-        print("Security : High(Currently secure)")
-    
+        print("Hash Type  : SHA-512")
+        print("Security   : High(Currently secure)")
+        return "SHA-512"
+
     else:
         print("Valid hexadecimal string, but unknown hash type.")
+        return "Unknown"
 
 print("=== Hash Identifier ===")
 print("1. Analyze Single Hash")
@@ -56,14 +63,18 @@ choice = input("\nChoose an option (1 or 2): ")
 
 if choice == "1":
     hash_value = input("\nEnter hash: ")
-    analyze_hash(hash_value)
+    hash_type=analyze_hash(hash_value)
+   
 
 elif choice == "2":
     file=open("hashes.txt","r")
+    report = open("report.txt", "w")
 
     for line in file:
+        report.write(line)
         analyze_hash(line.strip())
 
+    report.close()
     file.close()
 
 else:
