f5f4e8b9cc9f115ca7464b4eb93f205f0011e109
Shivani557-eng <shivanimourya9955@gmail.com>
2026-07-12 22:14:12 +0530

    Integrated VirusTotal API for hash reputation lookup

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import requests
+from config import API_KEY
+
 valid_chars = "0123456789abcdefABCDEF" 
 
 hash_info = {
@@ -9,6 +12,39 @@ hash_info = {
     128: ("SHA-512", "High (Currently secure)")
 }
 
+def check_virustotal(hash_value):
+    url = "https://www.virustotal.com/api/v3/files/" + hash_value
+    headers = {
+        "x-apikey": API_KEY
+    }
+
+    response = requests.get(url, headers=headers)
+   
+    if response.status_code != 200:
+        print("\nVirusTotal Analysis")
+        print("-" * 25)
+        print("Could not analyze this hash.")
+        return
+    
+
+    data = response.json()
+    stats = data["data"]["attributes"]["last_analysis_stats"]
+    print("\nVirusTotal Analysis")
+    print("-" * 25)
+    print("Malicious :", stats["malicious"])
+    print("Harmless  :", stats["harmless"])
+    print("Suspicious:", stats["suspicious"])
+    print("Undetected:", stats["undetected"])
+
+    if stats["malicious"] > 0:
+       print("\nVerdict: MALICIOUS")
+       print("Detected by", stats["malicious"], "security vendors.")
+
+    else:
+       print("\nVerdict: CLEAN")
+       print("No antivirus vendor detected this hash.")
+
+
 def analyze_hash(hash_value):
     length = len(hash_value)
 
@@ -19,7 +55,11 @@ def analyze_hash(hash_value):
             is_valid = False
 
     if not is_valid:
-        print("Invalid hash")
+        print("\n" + "=" * 40)
+        print("HASH ANALYSIS REPORT")
+        print("=" * 40)
+        print("Hash        :", hash_value)
+        print("Status      : Invalid hexadecimal hash")
         return "Invalid"
 
     print("\n" + "=" * 40)
@@ -51,22 +91,22 @@ choice = input("\nChoose an option (1 or 2): ")
 if choice == "1":
     hash_value = input("\nEnter hash: ")
     hash_type=analyze_hash(hash_value)
+
+    if hash_type != "Invalid":
+        check_virustotal(hash_value)
    
 
 elif choice == "2":
     try:
         file = open("hashes.txt", "r")
         report = open("report.txt", "w")
-
-
+    
         for line in file:
             hash_value = line.strip()
             hash_type = analyze_hash(hash_value)
-        
-            report.write("=" * 40 + "\n")
-            report.write("Hash: " + hash_value + "\n")
-            report.write("Type: " + hash_type + "\n\n")
-            report.write("=" * 40 + "\n\n")
+
+            if hash_type != "Invalid":
+                check_virustotal(hash_value)
 
         report.close()
         file.close()
